22a6886bed64d006b6cbfed591155df910bed2df
Deathwizaa <107788239+Deathwizaa@users.noreply.github.com>
2022-06-19 02:35:48 -0500

    Add files via upload

---
+++ producsum.js
@@ -0,0 +1,7 @@
+var N = prompt("Introduce un numero para multiplicar");
+var N2 = prompt("Introduce el segundo numero para multiplicar");
+var R;
+for(let i=0;i<N2;i++){
+    R=R+N;
+}
+document.write('El resultado de '+N+' x '+N2+' = '+R);
